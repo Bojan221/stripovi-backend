5d2076ad1ca8e8332c3186af9874b12e72833203
bojann122 <bojan.andzic3@gmail.com>
2026-03-25 14:33:57 +0100

    hero page

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const path = require("path")
 const fs = require("fs")
+const bcrypt = require("bcrypt");
 const getAllUsers = async (req, res) => {
   try {
     const user = req.user;
@@ -94,7 +95,20 @@ const changeProfilePicture = async (req, res) => {
 
 const updateUserEmail = async (req, res) => {
   try {
+    const id = req.user.id;
+    const { email } = req.body;
+ const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+ if(!regex.test(email)) {
+  return res.status(400).json({ message: "Neispravan email!" });
+ }
 
+ const existingUser = await User.findOne({ email });
+  if(existingUser) {
+    return res.status(409).json({ message: "Korisnik sa ovim emailom već postoji!" });
+  }
+
+  const user = await User.findByIdAndUpdate(id, { email }, { returnDocument: 'after', runValidators: true });
+  res.status(200).json({ message: "Email je uspješno promijenjen!", user });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -102,7 +116,27 @@ const updateUserEmail = async (req, res) => {
 
 const updateUserPassword = async (req, res) => {
   try {
+    const id = req.user.id;
+    const { currentPassword, newPassword, confirmNewPassword } = req.body;
+    if (newPassword !== confirmNewPassword) {
+      return res.status(400).json({ message: "Lozinke se ne podudaraju!" });
+    }
 
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "Korisnik nije pronađen!" });
+    }
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Trenutna lozinka nije ispravna!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Lozinka je uspješno promijenjena!" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
